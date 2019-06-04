e0b05dc1d984f02b8bb367560444316c8274756f
Nathan <n8benzor@gmail.com>
2019-06-04 14:47:12 -0700

    Final Update for working game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,13 @@
 // created an array with all of the letters the user can chose
-var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "_"];
+let letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "_"];
 // orginal game settings 
-var wins = 0;
-var losses = 0;
-var guessesLeft = 9;
-var guessedLetters = [];
-var letterToGuess = null;
+let wins = 0;
+let losses = 0;
+let guessesLeft = 9;
+let guessedLetters = [];
+let letterToGuess = null;
 //computer randomly chooses a letter
-var computerGuess = letters[Math.floor(Math.random() * letters.length)];
+let computerGuess = letters[Math.floor(Math.random() * letters.length)];
 
 console.log(computerGuess);
 
@@ -23,9 +23,9 @@ function addToGuessed() {
 
 // restart game function
 function restart() {
-    var guessesLeft = 9;
-    var guessedLetters = [];
-    var computerGuess = letters[Math.floor(Math.random() * letters.length)];
+     guessesLeft = 9;
+     guessedLetters = [];
+     computerGuess = letters[Math.floor(Math.random() * letters.length)];
 }
 
 countUserGuess();
@@ -33,7 +33,7 @@ countUserGuess();
 // users presses a key
 document.onkeyup = function(event) {
     guessesLeft--;
-    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+    userGuess = String.fromCharCode(event.keyCode).toLowerCase();
     guessedLetters.push(userGuess);
     countUserGuess();
     addToGuessed();
@@ -48,6 +48,7 @@ document.onkeyup = function(event) {
         document.querySelector("#losses").innerHTML = "losses: " + losses;
         restart();
     }
+    console.log(computerGuess);
 };
 
 // comparison between the users guess and the randomly generated computer guess
